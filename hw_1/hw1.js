b86c8942e9ce7fd70d9107ad44e174753da1c6b0
antonio63963 <shotvideo25@gmail.com>
2021-05-21 15:44:57 +0300

    momento mori

--- hw_1/hw1.js
+++ hw_1/hw1.js
@@ -1,1 +1,14 @@
-console.log('test hw 1');
+const moment = require('moment');
+const birth = moment([1978, 2, 25]);
+const current = moment();
+const millisecDiff = current.diff(birth);
+const age = moment.duration(millisecDiff)._data;
+console.log(age);
+
+// 2 variant to string
+// let res = ``;
+// for(key in age) {
+//   const string = `${key}: ${age[key]}, `;
+//   res += string;
+// }
+// console.log(res);
