5b95d2f21bf9ac0313073407597f8f8da0f64e8c
antonio63963 <shotvideo25@gmail.com>
2021-06-15 16:30:38 +0300

    links works

--- hw_7_routing/public/js/form.js
+++ hw_7_routing/public/js/form.js
@@ -4,22 +4,24 @@ const inputArr = [...form.querySelectorAll('input')];
 const url = 'http://localhost:3333/userData';
 
 const formSubmit = (url) => async (e) => {
-  e.preventDefault();
-  const userData = {};
-  inputArr.forEach(input => userData[input.className] = input.value);
-  console.log(userData);
+  if (e.target.classList.contains('submit')) {
+    e.preventDefault();
+    const userData = {};
+    inputArr.forEach(input => userData[input.className] = input.value);
+    console.log(userData);
 
-  axios.post('/userData', userData)
-  .then(function (response) {
-    console.log(response.status);
+    axios.post('/userData', userData)
+      .then(function (response) {
+        console.log(response.status);
 
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 }
 
 
-form.addEventListener('submit', formSubmit(url));
+form.addEventListener('click', formSubmit(url));
 
 
--- hw_7_routing/routes/mainRoute.js
+++ hw_7_routing/routes/mainRoute.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const router = express.Router();
-
+const registration = `
+<label class="userLable">
+  Your password: 
+  <input name="user" class="userPasswordrepeat" type="password" placeholder="****">
+</label>
+`
 const cont = {
-form:`
-<form id="form" name="user">
+  login: `
+    <form id="form" name="user">
+      <div class="formTop">
+        <h1>LOGIN</h1>
+      </div>
+      <label class="userLable">
+        Your email: 
+        <input name="user" class="userEmail" type="text" placeholder="some@mail.com">
+      </label>
+  
+      <label class="userLable">
+        Your name: 
+        <input name="user" class="userName" type="text" placeholder="your name">
+      </label>
+      <label class="userLable">
+        Your password: 
+        <input name="user" class="userPassword" type="password" placeholder="****">
+      </label>
+        <button class="submit btn" type="submit">SUBMIT</button>
+    </form>
+    
+      <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+      <script src="./form.js"></script>
+    `
+  ,
+
+  registration: `
+  <form id="form" name="user">
+    <div class="formTop">
+      <h1>REGISTRATION</h1>
+    </div>
     <label class="userLable">
       Your email: 
       <input name="user" class="userEmail" type="text" placeholder="some@mail.com">
@@ -18,29 +52,37 @@ form:`
       <input name="user" class="userPassword" type="password" placeholder="****">
     </label>
 
-    <button class="submit" type="submit">SUBMIT</button>
+    <label class="userLable">
+      Your password again: 
+      <input name="user" class="userPasswordrepeat" type="password" placeholder="****">
+    </label>
+
+    <button class="submit btn" type="submit">REGISTRATION</button>
   </form>
+  
+    <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+    <script src="./form.js"></script>
+  `
+  ,
 
-  <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
-  <script src="./form.js"></script>
-`,
+  mainPage: `
+      <h1 class="title">Hello NODE</h1>
+      <h2>Я вам ничего не покажу, пока вы не зарегистрируетесь!</h2>
+      <img class="img" src="./nodejsLogo.svg">
+    
+    `
 
-mainPage: `
-  <h1 class="title">Hello NODE</h1>
-  <p>Lorem ipsum dolor sit amet, consectet</p>
-  <img class="img" src="./nodejsLogo.svg">
-
-`
 }
 
 
 router.get('/', (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
   res.render('generic', { content: cont.mainPage })
 })
 
 router.get('/:id', (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
   res.render('generic', { content: cont[id] })
 })
+
 module.exports = router;
