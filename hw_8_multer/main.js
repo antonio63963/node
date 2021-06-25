d4ab5302928ba2cda5aaf746b13dd78fcc210b2f
antonio63963 <4access@ukr.net>
2021-06-25 09:08:51 +0300

    try to rename with multer not best variant

--- hw_8_multer/main.js
+++ hw_8_multer/main.js
@@ -7,7 +7,7 @@ const { promises: Fs} = require('fs');
 
 const upload = multer({dest: path.join(__dirname, 'public/upload')});
 const server = express();
-server.use(express.json());
+// server.use(express.json());
 server.use(express.static(path.join(__dirname, 'public/js')));
 server.use(express.static(path.join(__dirname, 'public/style')));
 
@@ -18,8 +18,23 @@ server.get('/form', (req, res) => {
   res.render('mainPage');
 })
 
+// порядок инпутов в разметке имеет значение иначе в req не попадут!!!
+
+
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './public/upload')
+//   },
+//   filename: function (req, file, cb) {
+//     console.log('FILE NAME repeat: ',req.body);
+//     console.log('FILE repeat: ', req.file);
+//     cb(null, req.body.filename);
+//   }
+// })
+// const upload = multer({ storage: storage })
+
 server.post('/form/req', upload.single('fileSend'), (req, res) => {
-  
+  console.log(req.body);
   const newName = req.body.fileName;
   const dir = './public/upload';
   const name = req.file.filename;
@@ -28,5 +43,5 @@ server.post('/form/req', upload.single('fileSend'), (req, res) => {
   res.json({status: 'ok'});
 })
 
-server.listen(1231);
+server.listen(1231, () => console.log('server start'));
 
