b7d6a1205a1049fbc79611011ab3bb347bfbef9a
antonio63963 <4access@ukr.net>
2021-07-06 13:02:40 +0300

    var to const

--- hw_11_generator/routes/index.js
+++ hw_11_generator/routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 const title = ` <h1> Введите id планет </h1>`;
