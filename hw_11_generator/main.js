3d1218fb304d5471a964eca0fd4882b42966a91d
antonio63963 <4access@ukr.net>
2021-07-10 16:18:39 +0300

    change

--- hw_11_generator/main.js
+++ hw_11_generator/main.js
@@ -3,7 +3,6 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-// const axios = require('axios');
 const getPlanets = require('./controllers/getPlanetsCtrl');
 
 const indexRouter = require('./routes/mainRouter');
