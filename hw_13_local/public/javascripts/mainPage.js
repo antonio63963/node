a26f9d0e7261a42f60d0194b3d9ef25d7e99a24c
antonio63963 <4access@ukr.net>
2021-07-11 16:54:24 +0300

    2level

--- hw_13_local/public/javascripts/mainPage.js
+++ hw_13_local/public/javascripts/mainPage.js
@@ -1,4 +1,6 @@
 const btnCardArr = [...document.querySelectorAll('.toStore')];
+const orderForm = document.forms.orderForm;
+const orderBtn = orderForm.querySelector('.submit');
 
 
 btnCardArr.forEach(btn => {
@@ -7,14 +9,34 @@ btnCardArr.forEach(btn => {
     const dataJson = localStorage.getItem('store') || "[]";
     const data = JSON.parse(dataJson);
     const existProd = data.find(prod => prod.id === id);
-
+    
     existProd ? 
     existProd.amount += 1 :
     data.push({id: id, amount: 1});
-
+    
     const storeJson = JSON.stringify(data);
-console.log(data);
+    console.log(data);
     localStorage.setItem('store', storeJson)
     
   })
-})
+});
+
+if(orderForm) {
+  orderForm.addEventListener('click', (e) => {
+    if(e.target.name !== 'deliveryMethods') return false;
+    const selectedMethod = e.target.value;
+    const postServices = orderForm.querySelectorAll('.delivery');
+    postServices.forEach(service => {
+      if(service.classList.contains(selectedMethod)) {
+        service.classList.remove('hidden');
+      } else {
+        service.classList.add('hidden')
+      }
+    })
+
+  })
+orderBtn.addEventListener('submit', (e) => {})
+  const formData = new FormData(orderForm);
+  // console.log(formData.getAll(deliveryMethod));
+
+}
