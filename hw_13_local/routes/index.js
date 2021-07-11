4bca7d832bcd54f62bedc159241063433453747b
antonio63963 <4access@ukr.net>
2021-07-11 15:04:44 +0300

    1 level

--- hw_13_local/routes/index.js
+++ hw_13_local/routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', productVis: true,});
 });
+router.get('/order', function(req, res, next) {
+  res.render('index', { title: 'Order', productVis: false})
+})
 
 module.exports = router;
