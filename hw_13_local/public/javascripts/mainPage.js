2fdb4bca0829aa3c5c9ce72bdfd6854f6ff1ce5e
antonio63963 <4access@ukr.net>
2021-07-13 13:05:18 +0300

    all the neccessary btns

--- hw_13_local/public/javascripts/mainPage.js
+++ hw_13_local/public/javascripts/mainPage.js
@@ -1,9 +1,6 @@
-const btnCardArr = [...document.querySelectorAll('.toStore')];
+const btnCardArr = [...document.querySelectorAll('.card')];
 const orderForm = document.forms.orderForm;
 const orderBtn = document.querySelector('.submit');
-const incAmountBtn = document.querySelector('.inc');
-const decAmountBtn = document.querySelector('.dec');
-const deleteProdBtn = document.querySelector('.del');
 
 function setStore(store) {
   const json = JSON.stringify(store);
@@ -16,30 +13,46 @@ function getStore() {
 };
 function addToStore(id) {
   const store = getStore();
+  console.log('add: ', store);
   const existProd = store.find(prod => prod.id === id);
     existProd ? 
     existProd.amount += 1 :
     store.push({id: id, amount: 1});
   setStore(store);
 };
+function decAmount(id) {
+  const store = getStore();
+  const existProd = store.find(prod => prod.id === id);
+    existProd ? 
+    existProd.amount -= 1 :
+    false;
+  setStore(store);
+};
 function removeFromStore(id) {
   const store = getStore();
   const ind = store.findIndex(item => item.id === id);
   if(ind !== -1) {
-    delete store[ind];
+    store.splice(ind, 1);
   }
   setStore(store);
 };
-function increaseStoreAmount(id) {
-
-}
 
 
 btnCardArr.forEach(btn => {
   btn.addEventListener('click', (e) => {
-    const {id} = e.target.dataset;
-  addToStore(id);
-
+    const {id} = e.currentTarget.dataset;
+    console.log(getStore());
+    // add or increase 
+    if(e.target.classList.contains('inc') || e.target.classList.contains('toStore')) {
+      addToStore(id);
+    }
+    // decrease amount
+    if(e.target.classList.contains('dec')) {
+      decAmount(id);
+    }
+    if(e.target.classList.contains('del')) {
+      removeFromStore(id);
+    }
   })
 });
 
@@ -63,12 +76,5 @@ orderBtn.addEventListener('click', async (e) => {
   const {data} = await axios.post(url, formData);
   console.log(data);
 });
-
-function getUnusefulElem(arr) {
-  const methodName = formData.get(`deliveryMethods`);
-  return arr.filter(elem => elem)
-}
-
-
 
 }
