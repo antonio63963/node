5a57ea51c026026fb5c3c9250f820f507f6d0801
antonio63963 <4access@ukr.net>
2021-07-20 20:29:01 +0300

    whithout css

--- hw_15_ws/main.js
+++ hw_15_ws/main.js
@@ -20,20 +20,46 @@ const server = http.createServer((req, res) => {
     sendFile(res, 'public/js/index.js', 'application/javascript')
     return;
   }
+  if(req.url === 'styles/style.css') {
+    sendFile(res, 'public/styles/style.css', 'text/css')
+    return;
+  }
   res.writeHead(404);
   res.end();
 })
 
 server.listen(3000, () => console.log(`SERVER START PORT 3000`));
+// data 
+const prodList = [
+  {id: 1, title: 'fox', price: 44, description: 'some description'},
+  {id: 2, title: 'dog', price: 55, description: 'some description'},
+  {id: 3, title: 'cat', price: 66, description: 'some description'}
+]
 
 //WS server
 
 wsServer = new WSServer.Server({server});
 
-wsServer.on('connection', (ws) => {
-  ws.on('message', (message) => {
-    console.log('recieved: ', message);
+wsServer.on('connection', (socket) => {
+  let count = 0;
+  let startRes = 'i feel good!';
+
+  socket.on('message', (message) => {
+    const req = JSON.parse(message);
+    console.log(req);
+
+    if(req.type == 'userNumber') {
+      count += Number(req.data);
+      console.log('recieved: ', req.data);
+      console.log('count: ', count);
+      res = 'good number, guy!'
+    }
+    if(req.type == 'prodList') {
+      console.log('you wanna list...');
+      res = JSON.stringify({type: 'prodList',data: prodList});
+      socket.send(res)
+    }
   })
 
-  ws.send('something');
+  socket.send(JSON.stringify(startRes));
 });
--- hw_15_ws/public/js/index.js
+++ hw_15_ws/public/js/index.js
@@ -1,11 +1,54 @@
 console.log('it works');
-
+const form = document.forms.userNum;
+const getListBtn = document.querySelector('.getListBtn');
+const container = document.querySelector('.container');
 const socket = new WebSocket('ws://localhost:3000');
 
-socket.addEventListener('open', (e) => {
-  socket.send('Hello Server!');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const num = formData.get('num');
+  const req = JSON.stringify({type: 'userNumber', data: num});
+  socket.send(req);
 });
+console.log(getListBtn);
+getListBtn.addEventListener('click', (e) => {
+  const req = JSON.stringify({type: 'prodList'})
+  socket.send(req)
+})
+
+// socket.addEventListener('open', (e) => {
+//   socket.send('Hello Server!');
+// });
 
 socket.addEventListener('message', (e) => {
   console.log('Message from server ', e.data);
-})
+  const res = JSON.parse(e.data);
+  if(res.type == 'prodList') {
+    buildProdList(res.data)
+  }
+});
+
+
+function buildProdList(data) {
+  if(Array.isArray(data)) {
+    const prodArr = data.reduce((acc, {id, title, description, price}) => {
+      acc += `
+      <div class="card"
+       data-id="${id}"
+       data-name="${title}"
+       data-price="${price}"
+       >
+      <h2>${title}</h2>
+      <p>${description}</p>
+      <span class="price">${price}</span>
+      <button class="add"> add to cart </button>
+      <br>
+      <hr>
+    </div>
+      `;
+      return acc;
+    }, '');
+    container.innerHTML = prodArr;
+  }
+}
