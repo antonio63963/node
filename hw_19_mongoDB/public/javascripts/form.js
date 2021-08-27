d351d3bb08f8c4c71439b2df600a5aad176a5cd3
unknown <shotvideo25@gmail.com>
2021-08-27 13:58:52 +0300

    mongo start

---
+++ hw_19_mongoDB/public/javascripts/form.js
@@ -0,0 +1,20 @@
+const formArticle = document.forms.article;
+const authorInput = document.querySelector('.article_author');
+const titleInput = document.querySelector('.article_title');
+const articleInput = document.querySelector('.article');
+const url = `/form`;
+
+formArticle.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const dateCreate = moment().toISOString();
+  console.log(dateCreate);
+  const formData = {
+    author: authorInput.value,
+    title: articleInput,
+    article: articleInput.value,
+    dateCreate
+  };
+  console.log(formData);
+  const data = await axios.post(url, formData);
+  console.log(data);
+})
