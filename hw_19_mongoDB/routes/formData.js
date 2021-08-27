eae3bfa5136b5b15771737ff2fc0cdfbbfe629d3
unknown <shotvideo25@gmail.com>
2021-08-27 15:16:36 +0300

    db connect

--- hw_19_mongoDB/routes/formData.js
+++ hw_19_mongoDB/routes/formData.js
@@ -5,6 +5,7 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
   console.log(req.body);
   // res.render('index', { title: 'Express' });
+  res.send('status 200');
 });
 
 module.exports = router;
