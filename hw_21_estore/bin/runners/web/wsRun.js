ac3e48b520a2c778a5cf3577900143ab6376d538
unknown <shotvideo25@gmail.com>
2021-09-14 12:04:02 +0300

    estore start

---
+++ hw_21_estore/bin/runners/web/wsRun.js
@@ -0,0 +1,18 @@
+const sio = require('socket.io');
+
+const runnerWS = (serverHttp) => {
+  const io = sio(serverHttp);
+console.log( 'ser port : ', serverHttp);
+  io.on('connection', socket => {
+    console.log('ws run log');
+    console.log(`Connect ID: ${socket.id}`);
+
+
+
+    socket.on('disconnect', () => {
+      console.log(`Disconect ID: ${socket.id}`);
+    })
+  })
+};
+
+module.exports = runnerWS;
