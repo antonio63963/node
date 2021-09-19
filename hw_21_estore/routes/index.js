27f78344ca219dfce18d8599d274a306fe052bb8
unknown <shotvideo25@gmail.com>
2021-09-19 11:10:22 +0300

    bootstrap layout

--- hw_21_estore/routes/index.js
+++ hw_21_estore/routes/index.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const { getAllLaptops } = require('../controllers/cont_laptop');
+const { getAllCategories } = require('../controllers/cont_category')
 
 /* GET home page. */
 router.get('/', async(req, res) => {
   const laptops = await getAllLaptops(10);
+  const categories = await getAllCategories();
 
-  res.render('index', { title: 'Express', products: laptops });
+  res.render('index', { title: 'Express', products: laptops, categories });
 
 });
 
