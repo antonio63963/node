a174f4b2d72b39c1b3526b7ecafd21cccca81584
unknown <shotvideo25@gmail.com>
2021-10-30 21:43:22 +0300

    start layouts

---
+++ hw_23_photoplatform/public/javascripts/home.js
@@ -0,0 +1,8 @@
+const btnLogin = document.querySelector('.login'),
+  btnSignUp = document.querySelector('.signUp');
+
+
+btnLogin.addEventListener('click', ()=> {
+  window.location = '/login';
+});
+console.log("home is working!!");
---
+++ hw_23_photoplatform/routes/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const  router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index');
+});
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+
+
+module.exports = router;
