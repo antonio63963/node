ae4f58459a6dd236a468d926d8756f68f85d46ae
unknown <shotvideo25@gmail.com>
2021-11-09 19:49:28 +0200

    replace photo works!!

--- hw_23_photoplatform/public/javascripts/userPanel/album.js
+++ hw_23_photoplatform/public/javascripts/userPanel/album.js
@@ -19,19 +19,8 @@ wrapperAlbum.addEventListener('click', (e) => {
   
 });
 
-const onFileInput = async (e) => {
-  const { uid } = document.querySelector('.wrapper-album').dataset;
-  const photoID = e.target.closest('label').dataset.photo_id;
-  const photoSrc = e.target.closest('figure').lastElementChild.getAttribute('src');
-  const albumID = e.target.closest('figure').dataset.album_id;
-  console.log("photoID: ", photoID, 'SRC: ', photoSrc);
-  const formData = new FormData();
-  formData.append('photoID', photoID);
-  formData.append('photoSrc', photoSrc);
-  formData.append('albumID', albumID);
-  formData.append('uid', uid);
-  formData.append('photoReplace', e.target.files[0], 'photo');
-  
+const onFileInput = async (formData) => {
+  formData.append('photoReplace', replacePhotoInp.files[0], 'photo');
   const { data } = await axios.post('/userPanel/replacePhoto', formData);
   if(!data.status == 'ok' ) {
     console.log('Replace photo: ', data);
@@ -42,11 +31,20 @@ const onFileInput = async (e) => {
 
 
 photoContainer.addEventListener('click', (e) => {
-  e.stopImmediatePropagation()
-  console.log('Target: ', e.target);
+  e.stopPropagation()
   if(e.target.classList.contains('replaceIcon')){
-    console.log("if::: ", e.target);
-    replacePhotoInp.addEventListener('change', async (e) => {
-      e.stopImmediatePropagation(); onFileInput(e)});
+    const { uid } = document.querySelector('.wrapper-album').dataset;
+    const photoID = e.target.closest('label').dataset.photo_id;
+    const photoSrc = e.target.closest('figure').lastElementChild.getAttribute('src');
+    const albumID = e.target.closest('figure').dataset.album_id;
+    console.log("photoID: ", photoID, 'SRC: ', photoSrc);
+    const formData = new FormData();
+    formData.append('photoID', photoID);
+    formData.append('photoSrc', photoSrc);
+    formData.append('albumID', albumID);
+    formData.append('uid', uid);
+    console.log('Target: ', e.target);
+    
+    replacePhotoInp.addEventListener('change', async () => onFileInput(formData))
   }
-})
+});
