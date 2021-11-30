87709523071b5af052856247e8ca26e0887d87ca
unknown <shotvideo25@gmail.com>
2021-11-30 19:30:37 +0200

    layout like the telegram

--- hw_24_react/raiting/src/components/StarRating.js
+++ hw_24_react/raiting/src/components/StarRating.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import { useState } from 'react';
 import { FaStar } from 'react-icons/fa';
 import Star from './Star.js';
+
 
 const createArray = length => [...Array(length)];
 
 export default function StarRating({ totalStars = 5 }) {
-  return createArray(totalStars).map((n, i) => <Star key={i} />);
+  const [ selectedStars, setSelectedStars ] = useState(0)
+  return (
+    <div style={{padding: "5px"}}>
+      {
+        createArray(totalStars).map((n, i) => <Star key={i} selected={ selectedStars > i } onSelect={() => setSelectedStars(selectedStars + 1)} style={ {backgroundColor: "lightblue"} }/>)
+      }
+      <p>
+        {selectedStars} of {totalStars}
+      </p>
+    </div>
+  )
 }
