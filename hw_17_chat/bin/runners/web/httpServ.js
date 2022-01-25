d4a26d26103cc4bd582821d89e98d1aadac5e18a
Anton Fomin <shotvideo25@gmai.com>
2022-01-25 16:50:06 +0200

    del empty file socket

--- hw_17_chat/bin/runners/web/httpServ.js
+++ hw_17_chat/bin/runners/web/httpServ.js
@@ -7,7 +7,7 @@ function run() {
      * Get port from environment and store in Express.
      */
 
-    var port = normalizePort(process.env.PORT || '3000');
+    var port = normalizePort(process.env.PORT || '5000');
     app.set('port', port);
 
     /**
@@ -19,7 +19,7 @@ function run() {
      * Listen on provided port, on all network interfaces.
      */
 
-    server.listen(port);
+    server.listen(port, () => console.log(`Server up on port ${port}`));
     server.on('error', onError);
     server.on('listening', onListening);
 
